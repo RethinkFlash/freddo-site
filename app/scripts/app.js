3c9af7c877c031999756763c529e56bf185dea93
Ken Malley <ken@rethinkcanada.com>
2013-09-17 09:46:55 -0700

    basic loading of thumbnails is completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,16 @@
 'use strict';
 
 angular.module('FreddoApp',[])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/admin', {
+				templateUrl: 'views/admin.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('FreddoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope,$http) {
+		$scope.albumData = [];
+
+		$scope.fetchAlbumData = function(modelURL) {
+			$http.get(modelURL)
+				.success(function(data,status,headers) {
+					console.log(status);
+					$scope.albumData = data;
+				})
+				.error(function() {
+					console.log("error");
+				});
+		};
+
+		$scope.fetchAlbumData('model/album_data.json');
+	});
